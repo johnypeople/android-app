9a32ad26dff7db1357863b8a338ccac2eb26252f
johnypeople <gjoao18@gmail.com>
2022-05-31 22:52:41 -0300

    Update

--- livraria/src/main/kotlin/com/livraria/controller/CustomerController.kt
+++ livraria/src/main/kotlin/com/livraria/controller/CustomerController.kt
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam as PathParam1
 
 @RestController
 @RequestMapping("/customer")
